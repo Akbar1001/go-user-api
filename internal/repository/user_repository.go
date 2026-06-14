a1850204df38758a451d8db76191b807badac14d
Akbar <akbar.code20@gmail.com>
2026-06-14 11:15:21 +0530

    Repository layer build successfully

--- internal/repository/user_repository.go
+++ internal/repository/user_repository.go
@@ -2,8 +2,9 @@ package repository
 
 import (
 	"context"
+	"time"
 
-	"go-user-api/db/sqlc/generated"
+	sqlc "go-user-api/db/sqlc/generated"
 )
 
 type UserRepository struct {
@@ -16,4 +17,66 @@ func NewUserRepository(
 	return &UserRepository{
 		queries: queries,
 	}
+}
+
+// Create user
+func (r *UserRepository) CreateUser(
+	name string,
+	dob time.Time,
+) (sqlc.User, error) {
+
+	return r.queries.CreateUser(
+		context.Background(),
+		sqlc.CreateUserParams{
+			Name: name,
+			Dob:  dob,
+		},
+	)
+}
+
+// Get User
+func (r *UserRepository) GetUser(
+	id int32,
+) (sqlc.User, error) {
+
+	return r.queries.GetUser(
+		context.Background(),
+		id,
+	)
+}
+
+//List Users
+func (r *UserRepository) ListUsers() ([]sqlc.User, error) {
+
+	return r.queries.ListUsers(
+		context.Background(),
+	)
+}
+
+// Delete User
+func (r *UserRepository) DeleteUser(
+	id int32,
+) error {
+
+	return r.queries.DeleteUser(
+		context.Background(),
+		id,
+	)
+}
+
+// Update User
+func (r *UserRepository) UpdateUser(
+	id int32,
+	name string,
+	dob time.Time,
+) (sqlc.User, error) {
+
+	return r.queries.UpdateUser(
+		context.Background(),
+		sqlc.UpdateUserParams{
+			ID:   id,
+			Name: name,
+			Dob:  dob,
+		},
+	)
 }
